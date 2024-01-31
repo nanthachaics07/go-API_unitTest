8893e3e8a57fc9a16db7d5e13366b95acbb3e0cb
Nanthachai.CS07 <botsafe002@gmail.com>
2024-01-31 11:46:56 +0700

    fix testMain

--- main.go
+++ main.go
@@ -1,1 +1,14 @@
 package main
+
+func Add(a, b int) int {
+	return a + b
+}
+
+func Sub(a, b int) int {
+	return a - b
+}
+
+func main() {
+	println(Add(2, 3))
+	println(Sub(1, 2))
+}
--- main_test.go
+++ main_test.go
@@ -5,5 +5,8 @@ import (
 )
 
 func TestMain(t *testing.T) {
-
+	result := Add(2, 3)
+	if result != 5 {
+		t.Errorf("Add(2, 3) = %d, want 5", result)
+	}
 }
