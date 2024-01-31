f7d7265fea981675cea1d8e9bc335f155026dce5
Nanthachai.CS07 <botsafe002@gmail.com>
2024-01-31 11:57:52 +0700

    test validator

--- main_test.go
+++ main_test.go
@@ -1,12 +1,54 @@
 package main
 
 import (
-	testing "testing"
+	"bytes"
+	"encoding/json"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestMain(t *testing.T) {
-	result := Add(2, 3)
-	if result != 5 {
-		t.Errorf("Add(2, 3) = %d, want 5", result)
+func TestUserRoute(t *testing.T) {
+	app := setup()
+
+	// Define test cases
+	tests := []struct {
+		description  string
+		requestBody  User
+		expectStatus int
+	}{
+		{
+			description:  "Valid input",
+			requestBody:  User{"jane.doe@example.com", "Jane Doe", 30},
+			expectStatus: fiber.StatusOK,
+		},
+		{
+			description:  "Invalid email",
+			requestBody:  User{"invalid-email", "Jane Doe", 30},
+			expectStatus: fiber.StatusBadRequest,
+		},
+		{
+			description:  "Invalid fullname",
+			requestBody:  User{"jane.doe@example.com", "12345", 30},
+			expectStatus: fiber.StatusBadRequest,
+		},
+		{
+			description:  "Invalid age",
+			requestBody:  User{"jane.doe@example.com", "Jane Doe", -5},
+			expectStatus: fiber.StatusBadRequest,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.description, func(t *testing.T) {
+			reqBody, _ := json.Marshal(test.requestBody)
+			req := httptest.NewRequest("POST", "/users", bytes.NewReader(reqBody))
+			req.Header.Set("Content-Type", "application/json")
+			resp, _ := app.Test(req)
+
+			assert.Equal(t, test.expectStatus, resp.StatusCode)
+		})
 	}
 }
---
+++ manual.md
@@ -0,0 +1,1 @@
+RUN : go test
